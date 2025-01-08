a2b21b5cf500d25b8943f81625069e81079ef8cf
Coderzz69 <23mcce21@uohyd.ac.in>
2025-01-08 10:32:44 +0530

    Added Squares & Cubes

---
+++ 08-01-2025/Squares_Cubes.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Squares_Cubes{
+
+    static void generateSquareCubeTable(int n){
+
+        System.out.println("n_Val\tSquare\tCubes");
+
+        for (int i = 1; i <= n; i++){
+            System.out.println(i + "\t" + i*i + "\t" + i*i*i);
+        }
+
+    }
+    public static void main(String[] args){
+
+        int n;
+
+        try(Scanner in = new Scanner(System.in)){
+            n = in.nextInt();
+        }
+
+        generateSquareCubeTable(n);
+    }
+}
